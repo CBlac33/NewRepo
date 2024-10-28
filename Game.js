7c3910e5fff27ab5052b412b8637f51877b830fd
CBlac33 <clwsh3@gmail.com>
2024-10-28 14:56:15 -0400

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,31 @@
+function firstFunc()
+{
+ // this is a comment until the eol   
+    alert("you have created your first function! Yeah"); //comment from here to end
+    /* Cleveland Washington
+    line 2
+    line 3
+    */
+
+    function average(num1, num2)
+    {
+    var avg = (num1 + num2)/2.0;
+    alert("average = "+ avg);
+    }
+functionplayCraps()
+}
+    var play1 = Math.ceil(Math.random()*3);
+    var play2 = Math.ceil(Math.random()*3); 
+    var crapsImage = "craps_image.jpg";
+    var paperImage = "paper_image.png";
+var scissorImage = "scissor_image.png";
+document.getElementById("finalResult").innerHTML ="updates"
+if (player1 == 1)
+    {
+    document.getElementById("player1Img").src = craps.jpgImage;
+    }
+    "Else if  die1 = die2 and die1 % 2 = 0"
+    {
+     document.getElementById("player1Img").src = paperImage;
+
+    }
